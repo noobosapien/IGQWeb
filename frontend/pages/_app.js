3ed915186c5c6a625d6c733269a008a21c9d9cc2
mitnikkozo <migirulz44@gmail.com>
2022-07-05 19:27:48 +1200

    Initial

---
+++ frontend/pages/_app.js
@@ -0,0 +1,52 @@
+import '../styles/globals.css';
+import * as React from 'react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    window.gtag('config', 'UA-223410216-1', {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
---
+++ frontend/pages/index.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+
+export default function Home({ featured }) {
+  useEffect(() => {}, []);
+
+  return <>IGQuacker</>;
+}
